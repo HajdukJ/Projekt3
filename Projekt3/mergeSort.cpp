37845f948e01c317f67e8f79ab5351b8c4b7df2a
Jakub <hajduk.jakub7@wp.pl>
2024-11-09 21:54:37 +0100

    dodanie komentarzy do pliku mergeSort.cpp

--- Projekt3/mergeSort.cpp
+++ Projekt3/mergeSort.cpp
@@ -5,7 +5,7 @@ void sortowanie::scalanie(int tab[], int lewa, int srodek, int prawa) {
 	int x = lewa;
 	int y = srodek + 1;
 	int i = lewa;
-	int* pomocnicza = new int[prawa - lewa + 1];
+	int* pomocnicza = new int[prawa - lewa + 1];   // <--- Pomocnicza tablica, ktora bedzie przechowywac elementy do posortowania
 	for (int j = lewa; j <= prawa; j++) {
 		pomocnicza[j - lewa] = tab[j];
 	}
@@ -27,15 +27,15 @@ void sortowanie::scalanie(int tab[], int lewa, int srodek, int prawa) {
 		tab[i++] = pomocnicza[y - lewa];
 		y++;
 	}
-	delete[] pomocnicza;
+	delete[] pomocnicza;   // <--- Zwolnienie pamieci dla pomocniczej tablicy
 };
 void sortowanie::sortowanie_przez_scalanie(int tab[], int lewa, int prawa) {
-	if (lewa >= prawa) {
-		return;
+	if (lewa >= prawa) {   // <--- Jesli nie ma juz elementow do sortowania, zawiera tylko jeden element
+		return;   // <--- Zakoncz dzialanie 
 	}
 	int srodek;
-	srodek = (prawa + lewa) / 2;
-	sortowanie_przez_scalanie(tab, lewa, srodek);
-	sortowanie_przez_scalanie(tab, srodek + 1, prawa);
-	scalanie(tab, lewa, srodek, prawa);
+	srodek = (prawa + lewa) / 2;   // <--- Wyliczanie srodka w tablicy, by podzielic ja na dwie mniejsze czesci
+	sortowanie_przez_scalanie(tab, lewa, srodek);   // <--- Rekurencyjnie sortuje lewa czesc
+	sortowanie_przez_scalanie(tab, srodek + 1, prawa);   // <--- Rekurencyjnie sortuje prawa czesc
+	scalanie(tab, lewa, srodek, prawa);   // <--- Scala obie posortowane czesci w porzadku rosnacym
 };
