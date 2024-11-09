854ba0a2874dae0122591906927a51b24618d577
Jakub <hajduk.jakub7@wp.pl>
2024-11-09 21:37:09 +0100

    dodanie metod sortowania przez scalanie

--- Projekt3/main.cpp
+++ Projekt3/main.cpp
@@ -3,5 +3,13 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int tab[] = { 41, 45, 5, 19, 18, 45, 45, 24, 192, 204 };
+	int n = 10;
+	sortowanie sort;
+	sort.sortowanie_przez_scalanie(tab, 0, n - 1);
+	for (int i = 0; i < n; i++) {
+		std::cout << tab[i] << " ";
+	}
+	std::cout << std::endl;
+	return 0;
 }
--- Projekt3/mergeSort.cpp
+++ Projekt3/mergeSort.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 #include "mergeSort.h"
 
-void scalanie(int tab[], int lewa, int srodek, int prawa) {
-
+void sortowanie::scalanie(int tab[], int lewa, int srodek, int prawa) {
+	int x = lewa;
+	int y = srodek + 1;
+	int i = lewa;
+	int* pomocnicza = new int[prawa - lewa + 1];
+	for (int j = lewa; j <= prawa; j++) {
+		pomocnicza[j - lewa] = tab[j];
+	}
+	while (x <= srodek) {
+		if (y > prawa) {
+			tab[i++] = pomocnicza[x - lewa];
+			x++;
+		}
+		else if (pomocnicza[x - lewa] <= pomocnicza[y - lewa]) {
+			tab[i++] = pomocnicza[x - lewa];
+			x++;
+		}
+		else {
+			tab[i++] = pomocnicza[y - lewa];
+			y++;
+		}
+	}
+	while (y <= prawa) {
+		tab[i++] = pomocnicza[y - lewa];
+		y++;
+	}
+	delete[] pomocnicza;
 };
-void sortowanie_przez_scalanie(int tab[], int lewa, int prawa) {
-
+void sortowanie::sortowanie_przez_scalanie(int tab[], int lewa, int prawa) {
+	if (lewa >= prawa) {
+		return;
+	}
+	int srodek;
+	srodek = (prawa + lewa) / 2;
+	sortowanie_przez_scalanie(tab, lewa, srodek);
+	sortowanie_przez_scalanie(tab, srodek + 1, prawa);
+	scalanie(tab, lewa, srodek, prawa);
 };
