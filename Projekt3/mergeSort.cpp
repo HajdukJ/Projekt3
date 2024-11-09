9589b2e8814e08d1e9bb74ea168adab5d9eb8629
Jakub <Jakub@DESKTOP-E9K5LU2>
2024-11-09 13:04:30 +0100

    Dodaj pliki projektów.

---
+++ Projekt3/mergeSort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "mergeSort.h"
+
+void scalanie(int tab[], int lewa, int srodek, int prawa) {
+
+};
+void sortowanie_przez_scalanie(int tab[], int lewa, int prawa) {
+
+};
---
+++ Projekt3/mergeSort.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class sortowanie {
+private:
+
+public:
+	void scalanie(int tab[], int lewa, int srodek, int prawa);
+	void sortowanie_przez_scalanie(int tab[], int lewa, int prawa);
+};
