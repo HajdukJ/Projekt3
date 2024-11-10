1714232097bad8f1a2452f4e3f060d66ae492df4
Jakub <hajduk.jakub7@wp.pl>
2024-11-10 12:59:41 +0100

    .

--- Projekt3/mergeSort.cpp
+++ Projekt3/mergeSort.cpp
@@ -2,30 +2,31 @@
 #include "mergeSort.h"
 
 void sortowanie::scalanie(int tab[], int lewa, int srodek, int prawa) {
-	int x = lewa;
-	int y = srodek + 1;
-	int i = lewa;
+	int x = lewa;   // <--- Poczatek lewej czesci tablicy
+	int y = srodek + 1;   // <--- Poczatek prawej czesci tablicy
+	int i = lewa;   // <--- Miejsce w tablicy, gdzie wstawimy posortowany element
 	int* pomocnicza = new int[prawa - lewa + 1];   // <--- Pomocnicza tablica, ktora bedzie przechowywac elementy do posortowania
 	for (int j = lewa; j <= prawa; j++) {
-		pomocnicza[j - lewa] = tab[j];
+		pomocnicza[j - lewa] = tab[j];   // <--- Kopiowanie danych z tablicy do tablicy pomocniczej
 	}
-	while (x <= srodek) {
-		if (y > prawa) {
-			tab[i++] = pomocnicza[x - lewa];
-			x++;
+	/// Scalanie elementow z lewej oraz prawej czesci tablicy
+	while (x <= srodek) {   // <--- Dopoki elementy sa w lewej czesci tablicy
+		if (y > prawa) {   // <--- Jesli prawa czesc jest pusta
+			tab[i++] = pomocnicza[x - lewa];   // <--- Przenosimy elementy z lewej czesci do glownej tablicy
+			x++;   // <--- Nastepnie przechodzimy do nastepnego elementu w lewej czesci
 		}
-		else if (pomocnicza[x - lewa] <= pomocnicza[y - lewa]) {
-			tab[i++] = pomocnicza[x - lewa];
-			x++;
+		else if (pomocnicza[x - lewa] <= pomocnicza[y - lewa]) {   // <--- Jesli element znajdujacy sie po lewej czesci jest mniejszy albo rowny prawemu
+			tab[i++] = pomocnicza[x - lewa];   // <--- Przenosimy mniejszy element z lewej czesci tablicy do glownej
+			x++;   // <--- Nastepnie przechodzimy do nastepnego elementu w lewej czesci
 		}
-		else {
-			tab[i++] = pomocnicza[y - lewa];
-			y++;
+		else {   // <--- W przeciwnym wypadku, jesli element z prawej czesci tablicy jest mniejszy 
+			tab[i++] = pomocnicza[y - lewa];   // <--- Przenosimy ten mniejszy element z prawej czesci tablicy do glownej
+			y++;   // <--- Przechodzimy do nastepnego elementu, ktory jest w prawej czesci
 		}
 	}
-	while (y <= prawa) {
-		tab[i++] = pomocnicza[y - lewa];
-		y++;
+	while (y <= prawa) {   // <--- Dopoki elementy sa w prawej czesci tablicy
+		tab[i++] = pomocnicza[y - lewa];   // <--- Przenosimy elementy, ktore pozostaly w prawej czesci tablicy
+		y++;   // <--- Przechodzimy do nastepnego elementu w prawej czesci
 	}
 	delete[] pomocnicza;   // <--- Zwolnienie pamieci dla pomocniczej tablicy
 };
